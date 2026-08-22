0809e3e41e9998a78d6bdbad367c0574dc85017b
tanxifei <3061298665@qq.com>
2026-08-22 10:21:27 +0800

    增加WL-MWB按键音

--- fabric/src/main/java/top/xfunny/mod/Keys.java
+++ fabric/src/main/java/top/xfunny/mod/Keys.java
@@ -2,10 +2,6 @@ package top.xfunny.mod;
 
 public interface Keys {
     String MOD_VERSION = "1.0.2-prerelease.3";
-<<<<<<< HEAD
-    String BUILD_TIME = "260822-0920+0800";
-=======
-    String BUILD_TIME = "260821-1051+0800";
->>>>>>> 829a5c8c81af0c96e55d75069f3977d4fcf84870
+    String BUILD_TIME = "260822-1020+0800";
     String API_URL = "https://api.modrinth.com/v2/project/yunzhu-transit-extension/version";
 }
