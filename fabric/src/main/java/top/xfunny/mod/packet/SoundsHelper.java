b79428e62fbb823e996466df0db013d2eff3e5cb
tanxifei <3061298665@qq.com>
2026-08-22 08:27:06 +0800

    降低WLMW音量

--- fabric/src/main/java/top/xfunny/mod/packet/SoundsHelper.java
+++ fabric/src/main/java/top/xfunny/mod/packet/SoundsHelper.java
@@ -319,7 +319,7 @@ public final class SoundsHelper {
                         blockPos,
                         SoundEvents.HITACHI_WLMW_BUTTON_1.get(),
                         SoundCategory.BLOCKS,
-                        0.8F,
+                        0.6F,
                         1.0F
                 );
                 break;
