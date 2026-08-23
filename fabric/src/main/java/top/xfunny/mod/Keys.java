388adc61af8173bb54c4c0da57303de6683b8dc2
tanxifei <3061298665@qq.com>
2026-08-23 18:57:55 +0800

    调整WLM*系列替换音频后的按键音量

--- fabric/src/main/java/top/xfunny/mod/Keys.java
+++ fabric/src/main/java/top/xfunny/mod/Keys.java
@@ -2,6 +2,6 @@ package top.xfunny.mod;
 
 public interface Keys {
     String MOD_VERSION = "1.0.2-prerelease.3";
-    String BUILD_TIME = "260823-1840+0800";
+    String BUILD_TIME = "260823-1850+0800";
     String API_URL = "https://api.modrinth.com/v2/project/yunzhu-transit-extension/version";
 }
--- fabric/src/main/java/top/xfunny/mod/packet/SoundsHelper.java
+++ fabric/src/main/java/top/xfunny/mod/packet/SoundsHelper.java
@@ -319,7 +319,7 @@ public final class SoundsHelper {
                         blockPos,
                         SoundEvents.HITACHI_WLMW_BUTTON_1.get(),
                         SoundCategory.BLOCKS,
-                        0.4F,
+                        0.6F,
                         1.0F
                 );
                 break;
